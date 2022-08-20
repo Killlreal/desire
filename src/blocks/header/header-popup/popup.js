6f7bead195a064e080118b42245bb57030b1af03
Killlreal <sopochkin.kirill@yandex.ru>
2022-08-20 15:36:47 +0300

    upgrade popup

--- src/blocks/header/header-popup/popup.js
+++ src/blocks/header/header-popup/popup.js
@@ -1,59 +1,39 @@
-// const btnOpen = document.querySelector(".js-header__btn");
-// const btnClose = document.querySelector(".js-popup__close");
-// const popup = document.querySelector(".popup");
-
-// btnOpen.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     popup.classList.add("is-open");
-// });
-// btnClose.addEventListener("click", () => {
-//     popup.classList.remove("is-open");
-// });
-
-const popupLinks = document.querySelectorAll(".popup-link");
-const body = document.querySelector("body");
+const popupLinks = document.querySelectorAll(".js-popup-link");
+const body = document.body;
 const lockPadding = document.querySelectorAll(".lock-padding");
 
 let unlock = true;
 
-const timeout = 800;
+const timeout = 400;
 
-if (popupLinks.length > 0) {
-    for (let index = 0; index < popupLinks.length; index++) {
-        const popupLink = popupLinks[index];
-        popupLink.addEventListener("click", function (e) {
-            const popupName = popupLink.getAttribute("href").replace("#", "");
-            const currentPopup = document.getElementById(popupName);
-            popupOpen(currentPopup);
-            e.preventDefault();
-        });
-    }
-}
+popupLinks.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        const popupName = item.getAttribute("href").replace("#", "");
+        const currentPopup = document.getElementById(popupName);
+        popupOpen(currentPopup);
+        e.preventDefault();
+    });
+});
 
 const popupCloseIcon = document.querySelectorAll(".js-popup__close");
 
-if (popupCloseIcon.length > 0) {
-    for (let index = 0; index < popupCloseIcon.length; ++index) {
-        const el = popupCloseIcon[index];
-        el.addEventListener("click", function (e) {
-            popupClose(el.closest(".popup"));
-            e.preventDefault();
-        });
-    }
-}
+popupCloseIcon.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        popupClose(item.closest(".popup"));
+        e.preventDefault();
+    });
+});
 
 function popupOpen(currentPopup) {
     if (currentPopup && unlock) {
-        // const popupActive = document.querySelector(".popup.is-open");
-        // console.log(popupActive);
+        const popupActive = document.querySelector(".popup.is-open");
         // если у нас в попапе ссылка на другой попап
-        // if(popupAcitve){
-        //     popupClose(popuAcitve, false);
-        // } else {
-        //     bodyLock();
-        // }
+        if(popupActive){
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
+        }
         currentPopup.classList.add("is-open");
-        body.classList.add("is-lock");
         // при клике на область вне попапа, закрывается popup
         currentPopup.addEventListener("click", function (e) {
             if (!e.target.closest(".popup__content")) {
@@ -63,15 +43,47 @@ function popupOpen(currentPopup) {
     }
 }
 
+
 function popupClose(popupActive, doUnlock = true) {
     if (unlock) {
         popupActive.classList.remove("is-open");
-        body.classList.remove("is-lock");
-        // if (doUnlock) {
-        //     bodyUnlock();
-        // }
+        if (doUnlock) {
+            bodyUnlock();
+        }
     }
 }
+
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+    lockPadding.forEach((item) =>{
+        item.style.lockPaddingRight = lockPaddingValue;
+    });
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add("is-lock");
+    unlock = false;
+    setTimeout(function() {
+        unlock = true;
+    }, timeout);
+}
+
+function bodyUnlock() {
+    setTimeout(function() {
+        lockPadding.forEach((item) =>{
+            item.style.lockPaddingRight = "0px";
+            console.log(item.style.lockPaddingRight);
+            
+        });
+        body.style.paddingRight = "0px";
+        body.classList.remove("is-lock");
+    }, timeout);
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+
 // закрываем попап на esc
 document.addEventListener("keydown", function (e) {
     const popupActive = document.querySelector(".popup.is-open");
