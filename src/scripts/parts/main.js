383ff15c85bf5639603e32cd0e6071d47d698b2e
Killlreal <sopochkin.kirill@yandex.ru>
2022-09-13 21:23:51 +0300

    heder fix

--- src/scripts/parts/main.js
+++ src/scripts/parts/main.js
@@ -1,12 +1,16 @@
 //TODO изменить импорты, чтобы не импортить целый модуль, а только нужную часть
 //TODO прописать для всех картинок width , height
-//TODO допилить popup 
+//TODO допилить popup
 //TODO gallery на prod выдает ошибку из-за того, что не понимает const, вот сука
 //TODO уменьшить размер vendors
 //TODO fix header
-import {initSwiper} from "../../blocks/home/slider/slider";
+import headerFix from "../../blocks/header/header";
+import { initSwiper } from "../../blocks/home/slider/slider";
 import "../../blocks/header/header-popup/popup";
 import mixitUp from "../../blocks/home/gallery/gallery";
 
+
+headerFix();
 initSwiper();
 mixitUp();
+
